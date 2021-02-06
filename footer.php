300cad165b79ee99e7e68fb73eee4025bca3dfb4
CutlerMcMartin <cutler.mcmartin@gmail.com>
2021-02-06 17:43:49 -0600

    Adding the theme documents from the template

---
+++ footer.php
@@ -0,0 +1,10 @@
+        </div> <!-- .container-->
+        <footer id="footer">
+            <div id="copyright">
+                &copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+            </div>
+        </footer> <!-- .container-->
+    </div> <!-- .wrapper-->
+    <?php wp_footer(); ?>
+    </body>
+</html>
